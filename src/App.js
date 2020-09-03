fc7d30e6151f3da013997d4db35bb916c6030e1c
Alex Ricardo <alex.ricardo1999@hotmail.com>
2020-09-03 12:59:56 -0300

    Consultando API Tmdb e retornando filmes no component APP

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import React from 'react';
-
+import React, {  useEffect } from 'react';
+import Tmdb from './Tmdb';
 
 function App() {
+
+  useEffect(() => {
+    const loadAll = async () => {
+      //Pegando a lista total
+      let list = await Tmdb.getHomeList();
+      console.log(list);
+    }
+
+    loadAll();
+  }, []);
+
   return (
     <>
       <h1>Hello World</h1>
