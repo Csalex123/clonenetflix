86b4b6e26f7486d3725c49f6120bc763a67c7a64
Alex Ricardo <alex.ricardo1999@hotmail.com>
2020-09-03 22:10:43 -0300

    Filmes de destaques dinamicos

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import Tmdb from './Tmdb';
 
-import MovieRow from './components/MovieRow'
+import MovieRow from './components/MovieRow';
+import FeaturedMovie from './components/FeaturedMovie';
+
+import './global.css';
 
 function App() {
 
   const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
 
   useEffect(() => {
+
     const loadAll = async () => {
-      //Pegando a lista total
-      let list = await Tmdb.getHomeList();
+      //Pegando a lista Total de filmes
+      const list = await Tmdb.getHomeList();
       setMovieList(list);
 
+      //Pegando o Filme de Destaque do cabeçalho,
+      const originals = list.filter((i) => i.slug === 'originals');
+      const randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+      const movie = originals[0].items.results[randomChosen];
+      const chosenInfo = await Tmdb.getMovieInfo(movie.id, 'tv');
+      setFeaturedData(chosenInfo);
     }
 
     loadAll();
@@ -20,10 +31,15 @@ function App() {
 
   return (
     <div className="page">
+
+      {featuredData &&
+        <FeaturedMovie item={featuredData} />
+      }
+      
       <section className="lists">
         {
           movieList.map((item, key) => (
-              <MovieRow key={key} title={item.title} items={item.items} />
+            <MovieRow key={key} title={item.title} items={item.items} />
           ))
         }
       </section>
