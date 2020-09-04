f7d4f778703f23b872396317fc385658f8f47ab0
Alex Ricardo <alex.ricardo1999@hotmail.com>
2020-09-03 22:36:53 -0300

    exibindo todas informações no component featuredMovie

--- src/components/FeaturedMovie/index.js
+++ src/components/FeaturedMovie/index.js
@@ -1,15 +1,44 @@
 import React from 'react';
 import './style.css';
 
-const FeaturedMovie = ({item}) => {
+const FeaturedMovie = ({ item }) => {
+
+    const firstData = new Date(item.first_air_date);
+    let genres = [];
+
+    for (let i in item.genres) {
+        genres.push(item.genres[i].name);
+    }
+
     return (
-        <section className="featured" style={{ 
+        <section className='featured' style={{
             backgroundSize: 'cover',
             backgroundPosition: 'center',
             backgroundImage: `url(https://image.tmdb.org/t/p/original/${item.backdrop_path})`
-         }}>
-            <div>{item.original_name}</div>
-        </section>
+        }}>
+            <div className='featured--vertical'>
+                <div className='featured--horizontal'>
+                    <div className='featured--name'>
+                        {item.original_name}
+                    </div>
+                    <div className='featured--info'>
+                        <div className='featured--points'>{item.vote_average} pontos</div>
+                        <div className='featured--year'>{firstData.getFullYear()}</div>
+                        <div className='featured--seasons'>{item.number_of_seasons} temporada {item.number_of_seasons != 1 && 's'}</div>
+                    </div>
+
+                    <div className='featured--description'>{item.overview}</div>
+
+                    <div className='featured--buttons'>
+                        <a href={`/watch/${item.id}`}>► Assistir</a>
+                        <a href={`/list/add/${item.id}`}>+ Minha Lista</a>
+                    </div>
+                    <div className='featured--genres'>
+                        <strong>Genêros: {genres.join(', ')}</strong>
+                    </div>
+                </div>
+            </div>
+        </section >
     )
 }
 
