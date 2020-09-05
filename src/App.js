3664f0f80038ffcf3cf39011cfb7a36f63d9b988
Alex Ricardo <alex.ricardo1999@hotmail.com>
2020-09-04 23:23:05 -0300

    scroll carrocel de filmes

--- src/App.js
+++ src/App.js
@@ -69,6 +69,12 @@ function App() {
           ))
         }
       </section>
+
+      <footer>
+        Feito com amor por Alex Ricardo <br />
+        Direitos de imagem para Netflix <br />
+        Dados pegos do site Themoviedb.org
+      </footer>
     </div>
   );
 }
--- src/components/FeaturedMovie/index.js
+++ src/components/FeaturedMovie/index.js
@@ -24,7 +24,7 @@ const FeaturedMovie = ({ item }) => {
                     <div className='featured--info'>
                         <div className='featured--points'>{item.vote_average} pontos</div>
                         <div className='featured--year'>{firstData.getFullYear()}</div>
-                        <div className='featured--seasons'>{item.number_of_seasons} temporada{item.number_of_seasons != 1 && 's'}</div>
+                        <div className='featured--seasons'>{item.number_of_seasons} temporada{item.number_of_seasons !== 1 && 's'}</div>
                     </div>
 
                     <div className='featured--description'>{item.overview}</div>
